a87fcc00e3c8cef437df821208cde3013d8be8b7
Saurav Bharti <sauravbharti38@gmail.com>
2024-03-19 22:40:13 +0530

    minor change

---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import logo from '../assets/logo.svg'
+import { FaXTwitter } from "react-icons/fa6";
+import { TfiEmail } from "react-icons/tfi";
+import line from '../assets/line.svg'
+const Home = () => {
+    return (
+        <div className='  md:pt-2.8rem md:pb-0 md:px-60 pb-44 pt-16.3rem px-8'>
+
+      
+        <div className='flex justify-center items-center flex-col  px-4 pt-4 gap-24 md:gap-7.5rem bg-custom-bg-color '>
+            <div className='md:mt-12 mt-9'>
+                <div className='flex items-center justify-center gap-4'>
+                    <img className='md:w-16 h-16 w-8 text-fontcolor' src={logo} alt="" />
+                    <p className='text-fontcolor md:text-[3.5rem] text-[2rem] font-[700] '>sigmoid</p>
+                </div>
+                <p className='sm:text-[1.7rem] text-fontcolor2 font-[600] mt-2 md:leading-customlineheight text-[1rem] md:text-[1.8rem] whitespace-nowrap'>integrated staking layer for AI</p>
+            </div>
+            <div className='flex justify-center items-center flex-col mt-28'>
+                <p className=' text-[2rem] md:text-[2.8rem] font-[700] text-fontcolor  '>Join Us</p>
+                <div className='flex sm:gap-10 md:gap-8 gap-2 items-center justify-center md:mb-4'>
+                    <a href="https://twitter.com/sigmoidwtf" className='flex items-center justify-center gap-2'>
+
+                        <FaXTwitter className='text-orange-700' />
+                       <p className='underline font-[600] text-fontcolor '> @sigmoidwtf</p>
+                    </a>
+                   <img className='h-4 md:h-6 w-8 font-[600]' src={line} alt="" />
+                    <a href="mailto:hi@sigmoid.wtf" className='flex items-center justify-center gap-2'>
+                        <TfiEmail className='text-orange-700' />
+                        <p className='underline font-[600] text-fontcolor '>hi@sigmoid.wtf</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "fontcolor":"#29361D",
+        "fontcolor2":"#606C38"
+      },
+      lineHeight:{
+        "customlineheight":"5rem",
+        "5rem":"5rem"
+      },
+      spacing:{
+        "26rem":"26rem",
+        "29rem":"29rem",
+        "33rem":"33rem",
+        "53rem":"53rem",
+        "2.8rem":"2.8rem",
+        "11.3rem":"11.3rem",
+        "16.3rem":"16.3rem",
+        "7.5rem":"7.5rem"
+      },
+      colors:{
+        "custom-bg-color":"#FEFAE0"
+      },
+      height:{
+        "100vh":"100vh"
+      },
+      
+    },
+  },
+  plugins: [],
+}
